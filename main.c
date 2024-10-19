251524af3b6051e33266c286a680e7a0b9c763fd
matija <matijazutic96@gmail.com>
2024-10-20 01:31:10 +0200

    different, more flexible audio playback

--- main.c
+++ main.c
@@ -20,9 +20,33 @@ void data_callback(ma_device* pDevice, void* pOutput, const void* pInput, ma_uin
 }
 
 int main() {
+
+ 	struct notcurses_options opts = {0}; // man notcurses_init(3)
+ 	nc = notcurses_init(&opts, stdout);
+ 	if(nc == NULL){
+ 		return EXIT_FAILURE;
+ 	}
+ 	stdplane = notcurses_stdplane(nc);
+
+ 	int ystd = 0, xstd = 0;
+ 	notcurses_stddim_yx(nc, &ystd, &xstd);
+ 	struct ncplane_options nopts = {
+ 		.y = ystd/3 +1,
+ 		.x = xstd/3 +1,
+ 		.rows = 150,
+ 		.cols = 150,
+ 		.name = "plot",
+ 		/* .resizecb = resize_cb, */
+ 		/* .flags = NCPLANE_OPTION_FIXED, */
+ 		.margin_b = 0,
+ 		.margin_r = 0,
+ 	};
+ 	barplane = ncplane_create(stdplane, &nopts);
+
     ma_device_config deviceConfig;
     ma_device device;
     ma_result result;
+    ma_sound sound;
 
     // Initialize the Miniaudio device configuration
     deviceConfig = ma_device_config_init(ma_device_type_playback);
@@ -54,13 +78,19 @@ int main() {
     /* engineConfig.pResourceManager = &myCustomResourceManager;   // <-- Initialized as some earlier stage. */
     result = ma_engine_init(&engineConfig, &engine);
 
-    printf("Playback started. Press Enter to exit...\n");
-    ma_engine_play_sound(&engine, "darkorundek.mp3", NULL);
-    getchar();
+   /* ma_engine_play_sound(&engine, "darkorundek.mp3", NULL); */
+
+    result = ma_sound_init_from_file(&engine, "darkorundek.mp3", 0, NULL, NULL, &sound);
+	if (result != MA_SUCCESS) {
+        return result;
+    }
+
+    ma_sound_start(&sound);
 
     // Stop and uninitialize the audio device
     ma_device_stop(&device);
     ma_device_uninit(&device);
+	ma_sound_uninit(&sound);
 
     return 0;
 }
